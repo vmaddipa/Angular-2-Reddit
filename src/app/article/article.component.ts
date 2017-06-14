3f3c1c884cd6c6c29e2daa3373f78c38c4f91882
Vamsi M <vamsi.maddipatla@a2-g.com>
2017-06-14 12:13:01 -0400

    Added Voteup and Votedown functions on article.model so as to follow law of demeter(Each unit should have only limited knowledge about other units)

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -17,12 +17,12 @@ export class ArticleComponent implements OnInit {
  }
 
  voteUp() {
- this.article.votes += 1;
+ this.article.voteUp();
  return false;
  }
 
  voteDown() {
- this.article.votes -= 1;
+ this.article.voteDown();
  return false;
  }
 
--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -7,5 +7,24 @@ constructor(title: string, link: string, votes?: number) {
  this.title = title;
  this.link = link;
  this.votes = votes || 0;
+}
+
+voteUp(): void {
+ this.votes += 1;
+}
+
+voteDown(): void {
+ this.votes -= 1;
+}
+
+ domain(): string {
+  try {
+  const link: string = this.link.split('//')[1];
+  return link.split('/')[0];
+  } catch (err) {
+  return null;
+  }
  }
+
+
 }
